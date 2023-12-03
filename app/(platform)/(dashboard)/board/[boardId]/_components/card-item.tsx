5f738d0ae392576ff8f0ee3c87cba18296864032
theomelgar <theomelgar@gmail.com>
2023-12-02 23:57:26 -0300

    feat: locally drag and drop lists and cards

--- app/(platform)/(dashboard)/board/[boardId]/_components/card-item.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/_components/card-item.tsx
@@ -1,25 +1,30 @@
-"use client"
+"use client";
 
 import { Card } from "@prisma/client";
+import { Draggable } from "@hello-pangea/dnd";
 
-interface CardItemProps{
-index: number;
-data: Card;
+interface CardItemProps {
+  index: number;
+  data: Card;
 }
 
-export const CardItem = ({
-index,
-data
-}:CardItemProps) =>{
-
-  return(
-    <div
-    role="button"
-    className="
-    truncate border-2 border-transparent
-   hover:border-black py-2 px-3 text-sm bg-white
-    rounded-md shadow-sm">
-      {data.title}
-    </div>
-  )
-}
+export const CardItem = ({ index, data }: CardItemProps) => {
+  return (
+    <Draggable draggableId={data.id} index={index}>
+      {(provided) => (
+        <div
+          {...provided.draggableProps}
+          {...provided.dragHandleProps}
+          ref={provided.innerRef}
+          role="button"
+          className="
+            truncate border-2 border-transparent
+          hover:border-black py-2 px-3 text-sm bg-white
+            rounded-md shadow-sm"
+        >
+          {data.title}
+        </div>
+      )}
+    </Draggable>
+  );
+};
